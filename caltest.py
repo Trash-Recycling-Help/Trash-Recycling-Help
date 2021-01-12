0541e09dfd3d64195376deaa2de2cfe065de385b
Trash-Recyling-Help <dolphins4all@gmail.com>
2021-01-12 17:48:15 -0500

    Added multiple events from one file

--- caltest.py
+++ caltest.py
@@ -1,9 +1,16 @@
 from ics import Calendar, Event
 c = Calendar()
-e = Event()
-e.name = "Take out only the trash!"
-e.begin = '2021-01-10 21:30:00'
-c.events.add(e)
-c.events
-with open('my.ics', 'w') as my_file:
+e0 = Event()
+e0.name = "Take out only the trash!"
+e0.begin = '2021-01-12 23:30:00'
+c.events.add(e0)
+e1 = Event()
+e1.name = "Take out the trash and recycling!"
+e1.begin = '2021-01-12 23:40:00'
+c.events.add(e1)
+e2 = Event()
+e2.name = "Take out only the trash!"
+e2.begin = '2021-01-12 23:50:00'
+c.events.add(e2)
+with open('..\\trash-recyling-help.github.io\\my.ics', 'w') as my_file:
     my_file.writelines(c)
