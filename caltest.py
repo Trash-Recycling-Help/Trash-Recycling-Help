ffb2414a3844b43d11dbffc2f00e475e0a0a53b4
Trash-Recyling-Help <dolphins4all@gmail.com>
2021-01-13 14:21:17 -0500

    added function, alarm and comments

--- caltest.py
+++ caltest.py
@@ -1,16 +1,24 @@
-from ics import Calendar, Event
+# Import Calendar and datetime modules
+from ics import Calendar, Event, DisplayAlarm
+from datetime import datetime, timedelta 
+
+# Function to create alarmed event and add to calendar
+def addevent(calendar, name, begin):
+    a = DisplayAlarm()
+    a.trigger = timedelta(minutes = -1) 
+    e0 = Event(alarms=[a])
+    e0.name = name
+    e0.begin = begin
+    calendar.events.add(e0)
+
+# Create calendar
 c = Calendar()
-e0 = Event()
-e0.name = "Take out only the trash!"
-e0.begin = '2021-01-12 23:30:00'
-c.events.add(e0)
-e1 = Event()
-e1.name = "Take out the trash and recycling!"
-e1.begin = '2021-01-12 23:40:00'
-c.events.add(e1)
-e2 = Event()
-e2.name = "Take out only the trash!"
-e2.begin = '2021-01-12 23:50:00'
-c.events.add(e2)
+
+# Add specific calendar events
+addevent(c,"Take out only the trash!", '2021-01-13 19:16:00')
+addevent(c,"Take out the trash and the recycling!", '2021-01-13 19:21:00')
+addevent(c,"Take out only the trash!", '2021-01-13 19:26:00')
+
+# Writing calendar file to disk
 with open('..\\trash-recyling-help.github.io\\my.ics', 'w') as my_file:
     my_file.writelines(c)
